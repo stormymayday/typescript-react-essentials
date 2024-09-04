82ff3f82401d0c0fcd8160ceadc06c6009d5969f
stormymayday <stormymayday7@gmail.com>
2024-09-04 16:03:16 -0700

    Props - Type Alias & Interface

--- src/starter/02-props/index.tsx
+++ src/starter/02-props/index.tsx
@@ -1,4 +1,16 @@
-function Component({ name, id }: { name: string; id: number }) {
+// Type Alias
+type ComponentProps = {
+    name: string;
+    id: number;
+};
+
+// Interface
+// interface ComponentProps {
+//     name: string;
+//     id: number;
+// }
+
+function Component({ name, id }: ComponentProps) {
     return (
         <div>
             <h2>Name: {name}</h2>
