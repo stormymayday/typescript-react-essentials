578e0ca59c8648a579515350346b614a72523666
stormymayday <stormymayday7@gmail.com>
2024-09-04 15:48:44 -0700

    Component Returns

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import Component from "./starter/01-return";
+
 function App() {
-  return (
-    <main>
-      <h2>React & Typescript</h2>
-    </main>
-  );
+    return (
+        <main>
+            <Component />
+        </main>
+    );
 }
 
 export default App;
