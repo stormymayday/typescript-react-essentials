9d70384cb07dba924a671556cd0edc35759605be
stormymayday <stormymayday7@gmail.com>
2024-09-04 18:20:21 -0700

    State - primitive types

--- src/starter/03-state/index.tsx
+++ src/starter/03-state/index.tsx
@@ -1,9 +1,24 @@
+import { useState } from "react";
+
 function Component() {
-  return (
-    <div>
-      <h2>React & Typescript</h2>
-      <h2>State</h2>
-    </div>
-  );
+    const [text, setText] = useState("shakeAndBake");
+    const [number, setNumber] = useState<number>(1);
+
+    return (
+        <div>
+            <h2 className="mb-1">State</h2>
+            <h3>Text: {text}</h3>
+            <h3>Number: {number}</h3>
+            <button
+                className="btn btn-center"
+                onClick={() => {
+                    setText("hello");
+                    setNumber(2);
+                }}
+            >
+                click me
+            </button>
+        </div>
+    );
 }
 export default Component;
