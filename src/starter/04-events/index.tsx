6ca395890949e6182c0c6a73a633cf1111e827d5
stormymayday <stormymayday7@gmail.com>
2024-09-04 19:08:35 -0700

    Events - callback & reference

--- src/starter/04-events/index.tsx
+++ src/starter/04-events/index.tsx
@@ -1,9 +1,36 @@
+import { useState } from "react";
+
 function Component() {
-  return (
-    <div>
-      <h2>React & Typescript</h2>
-      <h2>Events</h2>
-    </div>
-  );
+    const [text, setText] = useState("");
+    const [email, setEmail] = useState("");
+
+    // Reference Function
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        console.log(e.target.value);
+        setEmail(e.target.value);
+    };
+
+    return (
+        <section>
+            <h2>events example</h2>
+            <form className="form">
+                <input
+                    type="text"
+                    className="form-input mb-1"
+                    value={text}
+                    onChange={(e) => setText(e.target.value)}
+                />
+                <input
+                    type="email"
+                    className="form-input mb-1"
+                    value={email}
+                    onChange={handleChange}
+                />
+                <button type="submit" className="btn btn-block">
+                    submit
+                </button>
+            </form>
+        </section>
+    );
 }
 export default Component;
