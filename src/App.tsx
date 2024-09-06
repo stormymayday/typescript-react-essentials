2b0fc20afdb01201c4cca41b2a39c5ebd20a58ca
stormymayday <stormymayday7@gmail.com>
2024-09-05 21:20:17 -0700

    Challenge - Profile Card

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-import Component from "./starter/04-events";
+import Component from "./starter/05-challenge";
 
 function App() {
     return (
         <main>
-            <Component />
+            <Component type="basic" name="susan" />
+            <Component type="advanced" name="anna" email="anna@gmail.com" />
         </main>
     );
 }
--- src/starter/05-challenge/index.tsx
+++ src/starter/05-challenge/index.tsx
@@ -1,9 +1,36 @@
-function Component() {
-  return (
-    <div>
-      <h2>React & Typescript</h2>
-      <h2>Challenge</h2>
-    </div>
-  );
+type BasicComponentProps = {
+    type: "basic";
+    name: string;
+};
+
+type AdvancedComponentProps = {
+    type: "advanced";
+    name: string;
+    email: string;
+};
+
+type ProfileCardProps = BasicComponentProps | AdvancedComponentProps;
+
+function Component(props: ProfileCardProps) {
+    const { type, name } = props;
+
+    if (type === "basic") {
+        return (
+            <article className="alert alert-success">
+                <h2>Name: {name}</h2>
+            </article>
+        );
+    }
+
+    const { email } = props;
+
+    if (type === "advanced") {
+        return (
+            <article className="alert alert-danger">
+                <h2>Name: {name}</h2>
+                <h2>Email: {email}</h2>
+            </article>
+        );
+    }
 }
 export default Component;
